e493b4fbc4d12fdb417b1b2d79db51eaf3747cbb
khieem <48560151+khieem@users.noreply.github.com>
2021-10-14 22:17:49 +0700

    hoàn thành

--- lexer.cpp
+++ lexer.cpp
@@ -7,173 +7,236 @@
 
 using namespace std;
 
-/////// FILE PATH CONSTANTS ////////////
+
 const string SRC = "in.vc";
 const string DST = "out.vctok";
 const string DAT = "transition.dat";
 
 
-/////// GLOBAL VARIABLES ///////////
 set<string> keywords;
 string endstates[25];
-map<string, int> table[25]; // transition table, implemented as a map
-string source_code; // source code file as a string for convenience
-vector<string> tokens; // kết quả phân tích
 
-//////// FUNCTION PROTOTYPES //////////
-void load_data(); // đọc dữ liệu từ file .dat vào 3 bảng: keywords, endstates và table 
-void remove_comments(istream& file); // xóa bỏ comment từ "file" và lưu vào "source_code"
-string reduce(char c); // chuyển ký tự 'c' về dạng đầu vào DFA
-void write_output(string des = "out.vctok"); // ghi kết quả phân tích ra "des"
+// transition table, implemented as a map
+map<string, int> table[25];
+
+// source code file as a string for convenience
+string source_code;
+
+// kết quả phân tích
+vector<string> tokens;
+
+// đọc dữ liệu từ file .dat vào 3 bảng: keywords, endstates và table
+void load_data();
+
+// xóa bỏ comment từ "file" và lưu vào "source_code"
+void remove_comments( istream& file );
+
+// chuyển ký tự 'c' về dạng đầu vào DFA
+string reduce( char c );
+
+// ghi kết quả phân tích ra "des"
+void write_output( string des = "out.vctok" );
+
+// báo lỗi ở trạng thái "state" tại dòng "line"
+void inform_error_at( int line, int state, vector<char>& buffer );
 
 
-//////////// MAIN //////////////
-int main(int argc, char const *argv[])
+int main( int argc, char const *argv[] )
 {
 	ifstream file;
-	switch (argc)
+	switch ( argc )
 	{
 		case 1:
-			file.open(SRC);
-			if (!file.good())
+			file.open( SRC );
+			if ( !file.good() )
 			{
-				cerr << "Hay dat ma nguon trong file \"" << SRC << "\"";
+				cerr << "Put VC source code in \"" << SRC << "\"";
 				return -1;
 			}
 			break;
 		case 2:
-			file.open(argv[1]);
-			if (!file.good())
+			file.open( argv[ 1 ] );
+			if ( !file.good() )
 			{
-				cerr << "Khong tim thay \"" << argv[1] << "\"";
+				cerr << "Can not find \"" << argv[1] << "\"";
 				return -1;
 			}
 			break;
 	}
 	
 	load_data();	
-	remove_comments(file);
+	remove_comments( file );
 
 	int linecount = 1;
-	int i = 0, j = 0;
+	int j = 0;
 	char c;
 	int state = 0;
 	string input;
 	bool error = false;
 	vector<char> buffer;
-	while (c = source_code[j++])
-	{
-		if (int(c) < 0) break; // đừng đặt điều kiện này vào trong while
-		if (c == '\n') {
-			++linecount;
-			continue;
-		}
-		input = reduce(c);
 
-		if (table[state].count(input) == 0)	{
-			if (table[state].count("other") == 0) // k nhận input
+	while ( c = source_code[ j++ ] )
+	{
+		if ( int( c ) < 0 ) break; // đừng đặt điều kiện này vào trong while
+		if ( c == '\n' ) ++linecount;
+
+		input = reduce( c );
+		if ( table[ state ].count( input ) == 0 )
+		{
+			// tại state không nhận input => lỗi
+			if ( table[ state ].count( "other" ) == 0)
 			{
 				error = true;
-				cout << "Dong " << linecount << ". '" << string(buffer.begin(), buffer.end()) + c  << "'" << " is not recognized!" << endl;
+				
+				/* ngoại lệ tại state=4 với ký tự đọc được là 'e'
+				 * automat đợi 1 chữ số hoặc +- để hoàn thành dạng e_notation_float
+				 * tuy nhiên vẫn có thể chấp nhận chữ cái trong trường hợp
+				 * indentifier là "2efgh". điều kiện if này để xử lý
+				 * trường hợp đó, giúp hạn chế sửa đổi automat
+            */
+				if ( state == 4 )
+				{
+					// lấy lại ký tự ngay sau ký tự 'e'
+					buffer.push_back( source_code[ j-1 ] );
+
+					// tiếp tục lấy tất cả ký tự còn lại. vd 2ed[dfjegow] = 5;
+					while ( isalpha( source_code[ j ] ) || isdigit( source_code[ j ] ) )
+					{
+						buffer.push_back( source_code[ j++ ] );
+					}
+				}
+				
+				inform_error_at(linecount, state, buffer);
+
+				// khởi động lại automat
 				buffer.clear();
 				state = 0;
 				continue;
 			}
-			else // nhận input qua other
+			else // nhận input qua "other"
 			{
+				/**
+				 * xử lý indentifier không đúng
+				 * (bên trên là trường hợp riêng khi có 'e' ngay sau digit đứng đầu tiên)
+				 * bởi sau 'e' chỉ có thể nhận digit hoặc +-, gặp letter sẽ rơi vào if bên trên
+				 * trong khi sau digit có thể nhận "other", trong đó có cả letter
+				 * làm thế này để tránh phải sửa automat và sửa nhiều code, dù code sẽ bị lặp
+				 */
+				if ( state == 1 && ( input == "letter" || input == "E" ) )
+				{
+					error = true;
+					while ( isalpha( source_code[ j ] ) || isdigit( source_code[ j ] ) )
+					{
+						buffer.push_back( source_code[ j++ ] );
+					}
+					inform_error_at(linecount, state, buffer);
+					buffer.clear();
+					state = 0;
+					continue;
+				}
+				// nhận mọi ký tự nếu đang trong string literal
+				else if ( state == 11 )
+				{
+					buffer.push_back(c);
+					continue;
+				}
+				// nếu không thì lùi j đi 1 ký tự do trạng thái kết thúc đọc quá 1 ký tự
+				else --j;
 
-				state = table[state].at("other");
-				--j;
+				state = table[ state ][ "other" ];
 			}
 		}
 		else // nhận input bình thường
 		{
-			if (input != "ws") buffer.push_back(c);
-			state = table[state].at(input);
+			if ( input != "ws" ) buffer.push_back( c );
+			state = table[ state ][ input ];
 		}
-		string line;
-		if (endstates[state] != "") // state laf trang thai ket thuc
+
+		string pair, word;
+		if ( endstates[ state ] != "" ) // state là trạng thái kết thúc
 		{
-			string s = {buffer.begin(), buffer.end()};
-			if (s == "true" || s == "false")
+			word = { buffer.begin(), buffer.end() };
+			if ( word == "true" || word == "false" )
 			{
-				line = s + " " + "literal";
+				pair = word + " " + "literal";
 			}
-			else if (keywords.count(s))
+			else if ( keywords.count( word ) )
 			{
-				line = s + " " + "keyword";
+				pair = word + " " + "keyword";
 			}
-			else line = s + " " + endstates[state];
+			else pair = word + " " + endstates[ state ];
+
 			buffer.clear();
-			tokens.push_back(line);
+			tokens.push_back( pair );
 			state = 0;
 		}
-		// cout << c << state << " ";
 	}
 
-	if (!error)
-	write_output();
+	if ( !error )
+	{
+		write_output();
+		cout << "Sucessfully analyzed! Output written to \"" << DST << "\"" << endl;
+	}
 	return 0;
 }
 
 
-////// FUNCTION IMPLEMENTATIONS //////
 void load_data()
 {
 	ifstream file;
-	file.open(DAT);
-	if (!file.good())
+	file.open( DAT );
+	if ( !file.good() )
 	{
 		cerr << "Khong tim thay \"" << DAT << "\"";
-		exit(-1);
+		exit( -1 );
 	}
 	string line;
 	string word;
-	getline(file, line); // KEYWORDS
+	getline( file, line ); // KEYWORDS
 
-	getline(file, line); // boolean break continue else for float if int return void while
-	stringstream ss(line);
-	while (ss >> word)
-		keywords.insert(word);
+	getline( file, line ); // boolean break continue else for float if int return void while
+	stringstream ss( line );
+	while ( ss >> word )
+		keywords.insert( word );
 	ss.clear();
 
-	getline(file, line); // 
+	getline( file, line ); // 
 
-	getline(file, line); // ENDSTATES
+	getline( file, line ); // ENDSTATES
 	int state;
 	string attr;
-	while (getline(file, line) && line != "")
+	while ( getline( file, line ) && line != "" )
 	{
-		ss.str(line);
+		ss.str( line );
 		ss >> state >> attr;
-		endstates[state] = attr;
+		endstates[ state ] = attr;
 		ss.clear();
 	}
 
-	getline(file, line); // DFA
+	getline( file, line ); // DFA
 
-	regex pt("[\\w\\.\"<>!=|&+*/-]+ +\\d+"); // khớp với cặp "string int"
+	regex pt( "[\\w\\.\"<>!=|&+*/-]+ +\\d+" ); // khớp với cặp "string int"
 	smatch sm;
 
 	string input;
 	int output;
-	while (getline(file, line))
+	while ( getline( file, line ) )
 	{
-		state = stoi(line.substr(0, 2)); // state nằm ở đầu dòng
-		while (regex_search(line, sm, pt)) // tìm từng cặp input output
+		state = stoi( line.substr( 0, 2 ) ); // state nằm ở đầu dòng
+		while ( regex_search( line, sm, pt ) ) // tìm từng cặp input output
 		{
-			ss.str(sm[0]);
+			ss.str( sm[ 0 ] );
 			ss >> input >> output;
-			table[state][input] = output;
+			table[ state ][ input ] = output;
 			line = sm.suffix().str();
 			ss.clear();
 		}
 	}
 }
 
-void remove_comments(istream& file)
+void remove_comments( istream& file )
 {
-	source_code = {istreambuf_iterator<char>(file), istreambuf_iterator<char>{}};
+	source_code = { istreambuf_iterator<char>(file), istreambuf_iterator<char>{} };
 	int n = source_code.length();
 
 	/* CHÚ Ý. dùng mảng sao chép từng ký tự thay cho phép nối string
@@ -181,62 +244,68 @@ void remove_comments(istream& file)
 	chưa rõ có thể gây lỗi trong việc đếm dòng hay không. nếu có thì chuyển sang cách nối string
 	*/
 	// string res = "";
-	char res[n];
-	for (size_t i = 0; i < n; i++)
+	char res[ n ];
+	for ( size_t i = 0; i < n; i++ )
 		res[i] = ' ';	
 
 	bool single_cmt = false;
 	bool multi_cmt = false;
 
-	int last = 0;
-	for (int i = 0; i < n; ++i)
-	{
-		
+	for ( int i = 0; i < n; ++i )
+	{		
 		// đang trong single comment và gặp kết dòng
-		if (single_cmt == true && source_code[i] == '\n')
+		if ( single_cmt == true && source_code[i] == '\n' )
 		{
 			single_cmt = false;
 			// res += '\n';
-			res[i] = '\n';
+			res[ i ] = '\n';
 		}
 
 		// đang trong multi comment và gặp "*/"
-		else if (multi_cmt == true && source_code[i] == '*' && source_code[i+1] == '/')
+		else if ( multi_cmt == true && source_code[ i ] == '*' && source_code[ i+1 ] == '/' )
 			multi_cmt = false, i++;
 
 		// bỏ qua tất cả ký tự nằm trong comment, kể cả nnững comment khác
-		else if (single_cmt || multi_cmt)
+		else if ( single_cmt || multi_cmt )
 		{
-			if (source_code[i] == '\n')
+			if ( source_code[ i ] == '\n' )
 				// res += source_code[i];
-				res[i] = source_code[i];
+				res[ i ] = source_code[ i ];
 		}
-		else if (source_code[i] == '/' && source_code[i+1] == '/')
+		else if ( source_code[ i ] == '/' && source_code[ i+1 ] == '/' )
 			single_cmt = true, i++;
-		else if (source_code[i] == '/' && source_code[i+1] == '*')
+		else if ( source_code[ i ] == '/' && source_code[ i+1 ] == '*' )
 			multi_cmt = true, i++;
 
 		// giữ lại những ký tự không nằm trong comment
-		else res[i] = source_code[i];
+		else res[ i ] = source_code[ i ];
 		// else res += source_code[i];
 	}
 	source_code = res;
 }
 
-string reduce(char c)
+string reduce( char c )
 {
-	if (c == 'e' || c == 'E') return "E"; // e_notation_float
-	if ('a' <= c && c <= 'z' || 'A' <= c && c <= 'Z' || c == '_') return "letter";
-	if ('0' <= c && c <= '9') return "digit";
-	if (c == '{' || c == '}'|| c == '['|| c == ']' || c == '('|| c == ')'|| c == ';'|| c == ',')
+	if ( c == 'e' || c == 'E' ) return "E"; // e_notation_float
+	if ( 'a' <= c && c <= 'z' || 'A' <= c && c <= 'Z' || c == '_' ) return "letter";
+	if ( '0' <= c && c <= '9' ) return "digit";
+	if ( c == '{' || c == '}'|| c == '['|| c == ']' || c == '('|| c == ')'|| c == ';'|| c == ',' )
 		return "seperator";
-	if (c == ' ' || c == '\t' || c == '\n') return "ws";
-	return string(1, c);
+	if ( c == ' ' || c == '\t' || c == '\n' ) return "ws";
+	return string( 1, c );
 }
 
-void write_output(string des)
+void write_output( string des )
 {
-	ofstream file(des);
-	for (const string& s : tokens)
+	ofstream file( des );
+	for ( const string& s : tokens )
 		file << s << '\n';
+}
+
+void inform_error_at( int line, int state, vector<char>& buffer )
+{
+	cout << SRC << ": line " << line << ": \"" 
+	     << string( buffer.begin(), buffer.end() ) << "\"" 
+		  << " is not a valid " 
+		  << ( state != 21 && state != 23 ? "indentifier" : "operator" ) << endl;
 }
